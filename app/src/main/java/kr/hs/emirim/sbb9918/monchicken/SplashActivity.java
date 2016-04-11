fb5e9a9ca5a654e39c2daeca8eaec36d336477bd
Sebin Han <gkstpqls17@gmail.com>
2016-04-11 20:58:46 +0900

    #8 add splash code

---
+++ app/src/main/java/kr/hs/emirim/sbb9918/monchicken/SplashActivity.java
@@ -0,0 +1,29 @@
+package kr.hs.emirim.sbb9918.monchicken;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();//현재 activity를 마감
+            }
+
+        },3000);
+
+    }
+
+}
