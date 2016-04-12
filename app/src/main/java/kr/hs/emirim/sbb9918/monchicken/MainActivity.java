aa8396dda112fe2c520db62556e3295f54eac679
Sebin Han <gkstpqls17@gmail.com>
2016-04-12 20:07:13 +0900

    #10 add animation MainActivity to ResultActivity

--- app/src/main/java/kr/hs/emirim/sbb9918/monchicken/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/sbb9918/monchicken/MainActivity.java
@@ -1,6 +1,7 @@
 package kr.hs.emirim.sbb9918.monchicken;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,7 +21,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mName = (EditText) findViewById(R.id.name);
         mButton = (Button) findViewById(R.id.btn_show_me_the_chicken);
-        mButton.setOnClickListener(this);
+        mButton.setOnClickListener(this);//this -> 이벤트 헨들러 클래스의 객체가 들어감.
     }
 
     @Override
@@ -32,12 +33,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
+
         String name = mName.getText().toString();
        /*if (name.equals("")){
             Toast.makeText(this,"이름을 입력해 주세용", Toast.LENGTH_LONG).show();
 
         }else{
-            Toast.makeText(this,name+ "씨 배고파요!", Toast.LENGTH_LONG).show();
+            Toast.makeText(this,name+ "씨 배고파요!", Toast.                       LENGTH_LONG).show();
             Intent intent = new Intent(this, ResultActivity.class);
             intent.putExtra("Name", name);
             intent.putExtra("Age",18);
@@ -48,14 +50,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent intent = new Intent(this, ResultActivity.class);
             intent.putExtra("name", name);
             intent.putExtra("age",18);
-
             startActivity(intent);
 
+            overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
         }catch (NullPointerException e){
             Toast.makeText(this,"이름을 입력해 주세용", Toast.LENGTH_LONG).show();
         }catch (Exception e) {
             Toast.makeText(this,"뭔지 모르겠지만 잘 안돼네여", Toast.LENGTH_LONG).show();
         }
+
 
     }
 }
